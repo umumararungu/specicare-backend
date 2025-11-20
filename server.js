6257cb83fb4f94263fbd724b93d711963cae8dab
umumararungu <umcynth@gmail.com>
2025-11-20 21:20:52 +0300

    updated

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+app.set('trust proxy', 1); 
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
